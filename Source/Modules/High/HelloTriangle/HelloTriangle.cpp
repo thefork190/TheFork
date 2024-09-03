0f23d4e47df47779d93d915901c06493b9cc3ea5
Stan <theforkframework@gmail.com>
2024-09-02 21:36:50 -0700

    - adding vert/ind buffers

--- Source/Modules/High/HelloTriangle/HelloTriangle.cpp
+++ Source/Modules/High/HelloTriangle/HelloTriangle.cpp
@@ -2,9 +2,10 @@
 #define GLM_FORCE_LEFT_HANDED
 #include <glm/glm.hpp>
 #include <IResourceLoader.h>
-#include "Low/RHI.h"
-#include "HelloTriangle.h"
 #include "ILog.h"
+#include "Low/RHI.h"
+#include "Low/Window.h"
+#include "HelloTriangle.h"
 
 namespace HelloTriangle
 {
@@ -14,6 +15,10 @@ namespace HelloTriangle
         Shader* pTriShader = nullptr;
         RootSignature* pRootSignature = nullptr;
         DescriptorSet* pDescriptorSetUniforms = nullptr;
+        Pipeline* pPipeline = nullptr;
+        VertexLayout vertexLayout = {};
+        Buffer* pVertexBuffer = nullptr;
+        Buffer* pIndexBuffer = nullptr;
         std::vector<Buffer*> uniformsBuffers;
 
         // Uniforms data
@@ -65,6 +70,37 @@ namespace HelloTriangle
         removeDescriptorSet(pRenderer, passDataInOut.pDescriptorSetUniforms);
     }
 
+    static void AddPipeline(
+        Renderer* const pRenderer,
+        RHI::RHI const* pRHI,
+        Window::SDLWindow const* pWindow,
+        RenderPassData& passDataInOut)
+    {
+        RasterizerStateDesc rasterizerStateDesc = {};
+        rasterizerStateDesc.mCullMode = CULL_MODE_NONE;
+
+        RasterizerStateDesc rasterizeStateDesc = {};
+        rasterizeStateDesc.mCullMode = CULL_MODE_FRONT;
+
+        DepthStateDesc depthStateDesc = {};
+
+        PipelineDesc desc = {};
+        desc.mType = PIPELINE_TYPE_GRAPHICS;
+        GraphicsPipelineDesc& pipelineSettings = desc.mGraphicsDesc;
+        pipelineSettings.mPrimitiveTopo = PRIMITIVE_TOPO_TRI_LIST;
+        pipelineSettings.mRenderTargetCount = 1;
+        pipelineSettings.pDepthState = &depthStateDesc;
+        pipelineSettings.pColorFormats = &pWindow->pSwapChain->ppRenderTargets[0]->mFormat;
+        pipelineSettings.mSampleCount = pWindow->pSwapChain->ppRenderTargets[0]->mSampleCount;
+        pipelineSettings.mSampleQuality = pWindow->pSwapChain->ppRenderTargets[0]->mSampleQuality;
+        pipelineSettings.pRootSignature = passDataInOut.pRootSignature;
+        pipelineSettings.pShaderProgram = passDataInOut.pTriShader;
+        //pipelineSettings.pVertexLayout = &gSphereVertexLayout;
+        pipelineSettings.pRasterizerState = &rasterizeStateDesc;
+        pipelineSettings.mVRFoveatedRendering = true;
+        addPipeline(pRenderer, &desc, &passDataInOut.pPipeline);
+    }
+
     module::module(flecs::world& ecs)
     {
         ecs.import<RHI::module>();
@@ -94,6 +130,43 @@ namespace HelloTriangle
             ubDesc.ppBuffer = &renderPassData.uniformsBuffers[i];
             addResource(&ubDesc, nullptr);
         }
+
+        renderPassData.vertexLayout.mBindingCount = 1;
+        renderPassData.vertexLayout.mBindings[0].mStride = 12; // xyz pos
+        renderPassData.vertexLayout.mAttribCount = 1;
+        renderPassData.vertexLayout.mAttribs[0].mSemantic = SEMANTIC_POSITION;
+        renderPassData.vertexLayout.mAttribs[0].mFormat = TinyImageFormat_R32G32B32_SFLOAT;
+        renderPassData.vertexLayout.mAttribs[0].mBinding = 0;
+        renderPassData.vertexLayout.mAttribs[0].mLocation = 0;
+        renderPassData.vertexLayout.mAttribs[0].mOffset = 0;
+
+        std::vector<glm::vec3> triPositions(3);
+        triPositions[0] = { -0.5f, -0.5f , 0.f };
+        triPositions[1] = { 0.5f, -0.5f , 0.f };
+        triPositions[2] = { 0.5f, 0.5f , 0.f };
+
+        BufferLoadDesc sphereVbDesc = {};
+        sphereVbDesc.mDesc.mDescriptors = DESCRIPTOR_TYPE_VERTEX_BUFFER;
+        sphereVbDesc.mDesc.mMemoryUsage = RESOURCE_MEMORY_USAGE_GPU_ONLY;
+        sphereVbDesc.mDesc.mSize = 3 * 12;
+        sphereVbDesc.pData = triPositions.data();
+        sphereVbDesc.ppBuffer = &renderPassData.pVertexBuffer;
+        addResource(&sphereVbDesc, nullptr);
+
+        std::vector<uint32_t> triIndices(3);
+        triIndices[0] = 0;
+        triIndices[1] = 1;
+        triIndices[2] = 2;
+
+        BufferLoadDesc sphereIbDesc = {};
+        sphereIbDesc.mDesc.mDescriptors = DESCRIPTOR_TYPE_INDEX_BUFFER;
+        sphereIbDesc.mDesc.mMemoryUsage = RESOURCE_MEMORY_USAGE_GPU_ONLY;
+        sphereIbDesc.mDesc.mSize = sizeof(uint32_t) * 3;
+        sphereIbDesc.pData = triIndices.data();
+        sphereIbDesc.ppBuffer = &renderPassData.pIndexBuffer;
+        addResource(&sphereIbDesc, nullptr);
+
+        waitForAllResourceLoads();
 
         ecs.set<RenderPassData>(renderPassData);
     }
