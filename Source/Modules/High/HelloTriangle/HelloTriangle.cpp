78e90462c07fd28f0d04dd63eef189f43f5e9103
Stan <theforkframework@gmail.com>
2024-09-02 19:14:22 -0700

    - desc set, root sig, uniforms buffer cretion/destruction

--- Source/Modules/High/HelloTriangle/HelloTriangle.cpp
+++ Source/Modules/High/HelloTriangle/HelloTriangle.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#define GLM_FORCE_LEFT_HANDED
+#include <glm/glm.hpp>
 #include <IResourceLoader.h>
 #include "Low/RHI.h"
 #include "HelloTriangle.h"
@@ -9,6 +12,16 @@ namespace HelloTriangle
     struct RenderPassData
     {
         Shader* pTriShader = nullptr;
+        RootSignature* pRootSignature = nullptr;
+        DescriptorSet* pDescriptorSetUniforms = nullptr;
+        std::vector<Buffer*> uniformsBuffers;
+
+        // Uniforms data
+        struct UniformsData
+        {
+            glm::mat4 mvp;
+            glm::vec4 color;
+        } uniformsData;
     };
     
     static void AddShaders(Renderer* const pRenderer, RenderPassData& passDataInOut)
@@ -24,7 +37,33 @@ namespace HelloTriangle
         removeShader(pRenderer, passDataInOut.pTriShader);
     }
 
-    // 
+    static void AddRootSignature(Renderer* const pRenderer, RenderPassData& passDataInOut)
+    {
+        Shader* shaders[1];
+        uint32_t shadersCount = 0;
+        shaders[shadersCount++] = passDataInOut.pTriShader;
+
+        RootSignatureDesc rootDesc = {};
+        rootDesc.mShaderCount = shadersCount;
+        rootDesc.ppShaders = shaders;
+        addRootSignature(pRenderer, &rootDesc, &passDataInOut.pRootSignature);
+    }
+
+    static void RemoveRootSignature(Renderer* const pRenderer, RenderPassData& passDataInOut)
+    {
+        removeRootSignature(pRenderer, passDataInOut.pRootSignature);
+    }
+
+    static void AddDescriptorSet(Renderer* const pRenderer, RenderPassData& passDataInOut)
+    {
+        DescriptorSetDesc desc = { passDataInOut.pRootSignature, DESCRIPTOR_UPDATE_FREQ_NONE, 1 };
+        addDescriptorSet(pRenderer, &desc, &passDataInOut.pDescriptorSetUniforms);
+    }
+
+    static void RemoveDescriptorSet(Renderer* const pRenderer, RenderPassData& passDataInOut)
+    {
+        removeDescriptorSet(pRenderer, passDataInOut.pDescriptorSetUniforms);
+    }
 
     module::module(flecs::world& ecs)
     {
@@ -37,7 +76,25 @@ namespace HelloTriangle
         ASSERTMSG(pRHI, "RHI singleton doesn't exist.");
 
         RenderPassData renderPassData = {};
+        
         AddShaders(pRHI->pRenderer, renderPassData);
+        AddRootSignature(pRHI->pRenderer, renderPassData);
+        AddDescriptorSet(pRHI->pRenderer, renderPassData);
+
+        renderPassData.uniformsBuffers.resize(pRHI->dataBufferCount);
+        BufferLoadDesc ubDesc = {};
+        ubDesc.mDesc.mDescriptors = DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        ubDesc.mDesc.mMemoryUsage = RESOURCE_MEMORY_USAGE_CPU_TO_GPU;
+        ubDesc.mDesc.mFlags = BUFFER_CREATION_FLAG_PERSISTENT_MAP_BIT;
+        ubDesc.pData = nullptr;
+        for (uint32_t i = 0; i < pRHI->dataBufferCount; ++i)
+        {
+            ubDesc.mDesc.pName = "HelloTriangle_UniformBuffer";
+            ubDesc.mDesc.mSize = sizeof(RenderPassData::UniformsData);
+            ubDesc.ppBuffer = &renderPassData.uniformsBuffers[i];
+            addResource(&ubDesc, nullptr);
+        }
+
         ecs.set<RenderPassData>(renderPassData);
     }
 
@@ -45,9 +102,20 @@ namespace HelloTriangle
     {
         if (ecs.has<RHI::RHI>() && ecs.has<RenderPassData>())
         {
-            Renderer* pRenderer = ecs.get<RHI::RHI>()->pRenderer;
+            RHI::RHI const* pRHI = ecs.get<RHI::RHI>();
+
+            waitQueueIdle(pRHI->pGfxQueue);
+
+            Renderer* pRenderer = pRHI->pRenderer;
             RenderPassData* pRenderPassData = ecs.get_mut<RenderPassData>();
+            RemoveDescriptorSet(pRenderer, *pRenderPassData);
+            RemoveRootSignature(pRenderer, *pRenderPassData);
             RemoveShaders(pRenderer, *pRenderPassData);
+            
+            for (uint32_t i = 0; i < pRHI->dataBufferCount; ++i)
+            {
+                removeResource(pRenderPassData->uniformsBuffers[i]);
+            }
         }
     }
 }
