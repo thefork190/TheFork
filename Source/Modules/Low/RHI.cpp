f93c4f24871ce8c57bc4f9138c9dfdfe1d310285
Stan <theforkframework@gmail.com>
2024-09-06 21:48:56 -0700

    - reverted tmp change

--- Source/Modules/Low/RHI.cpp
+++ Source/Modules/Low/RHI.cpp
@@ -10,7 +10,6 @@ namespace RHI
     {
         RendererDesc rendDesc;
         memset(&rendDesc, 0, sizeof(rendDesc));
-        rendDesc.mEnableGpuBasedValidation = true;
         initRenderer(APP_NAME, &rendDesc, &pRenderer);
         ASSERT(pRenderer);
 
